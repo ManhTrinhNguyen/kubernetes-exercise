023af53d10bb614025894c9cfbc473fd1581a68a
Tim <nguyenmanhtrinh17041998@gmail.com>
2025-09-02 13:18:26 -0700

    update-python

--- monitoring-python/ssh-to-ec2.py
+++ monitoring-python/ssh-to-ec2.py
@@ -3,7 +3,7 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-private_key = paramiko.RSAKey.from_private_key_file('/Users/trinhnguyen/.ssh/terraform.pem')
+private_key = paramiko.RSAKey.from_private_key_file('/var/jenkins_home/terraform.pem')
 
 ssh.connect(hostname="13.57.49.64", username="ubuntu", pkey=private_key)
 
