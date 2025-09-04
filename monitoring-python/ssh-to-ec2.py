0bba52819a4f3d6cf64f2f325d4a97cec5d569f3
Tim <nguyenmanhtrinh17041998@gmail.com>
2025-09-04 11:22:52 -0700

    update-python

--- monitoring-python/ssh-to-ec2.py
+++ monitoring-python/ssh-to-ec2.py
@@ -6,9 +6,6 @@ host_ip = os.environ["HOST_IP"]
 ssh_path_key_file= os.environ["SSH_PATH_KEY_FILE"]
 ecr_password = os.environ["ECR_PASSWORD"]
  
-print(docker_image)
-print(ecr_password)
-
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -19,7 +16,7 @@ ssh.connect(hostname=host_ip, username="ubuntu", pkey=private_key)
 stdin, stdout, stderr = ssh.exec_command(f"echo {ecr_password} |docker login --username AWS --password-stdin 660753258283.dkr.ecr.us-west-1.amazonaws.com")
 print(stdout.readlines())
 
-# stdin, stdout, stderr = ssh.exec_command("docker run -p 8080:8080 -d run ${docker_image}")
-# print(stdout.readlines())
+stdin, stdout, stderr = ssh.exec_command("docker run -p 8080:8080 -d run ${docker_image}")
+print(stdout.readlines())
 
 ssh.close()
